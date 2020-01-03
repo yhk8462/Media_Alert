42978e45fb89f1898f1b9d3e2c49fd580c1b5eaf
yhk8462 <s3726115@rmit.edu.vn>
2020-01-03 10:37:00 +0700

    fixed sub page

---
+++ app/src/main/java/rmit/ad/mediaalert/SubGames.java
@@ -0,0 +1,146 @@
+package rmit.ad.mediaalert;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ActionMenuView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.os.Build.ID;
+
+public class SubGames extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+    NavigationView navigationView;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference myRef;
+    private GameList gameList;
+    private FirebaseAuth mAuth;
+    private String email="";
+    private String key="";
+    @SuppressLint("RestrictedApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sub_games);
+
+        drawerLayout = findViewById(R.id.drawer);
+        drawerLayout.addDrawerListener(toggle);
+        navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        final Button button = findViewById(R.id.btnSidebar);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
+
+        TextView header = findViewById(R.id.header);
+        TextView gameName = findViewById(R.id.game_name);
+        TextView gameDate = findViewById(R.id.game_date);
+        TextView gameCompany = findViewById(R.id.game_company);
+        TextView gamePlatform = findViewById(R.id.game_platform);
+        TextView gameDes = findViewById(R.id.game_des);
+        ImageView gameImg = findViewById(R.id.game_image);
+
+        //Get data and display ---------------------------------------------------------------------
+        final Intent details = getIntent();
+        final String name = (String) details.getExtras().get("name");
+        String date = (String) details.getExtras().get("date");
+        String company = (String) details.getExtras().get("company");
+        String des = (String) details.getExtras().get("des");
+        String platform = (String) details.getExtras().get("platform");
+        String imageURL = (String) details.getExtras().get("imageURL");
+        final String key = (String) details.getExtras().get("key");
+        gameList = new GameList(name, date, company, platform, des, imageURL);
+
+        header.setText(name);
+        Picasso.with(SubGames.this).load(imageURL).into(gameImg);
+        gameName.setText(name);
+        gameDate.setText("Release Date: " + "\n" + date);
+        gameCompany.setText("Produced by: " + "\n" + company);
+        gamePlatform.setText("Available on: " + "\n" + platform);
+        gameDes.setText(des);
+
+        //UnSub button -------------------------------------------------------------------------
+        mAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = firebaseDatabase.getReference().child("Users");
+        Button button1 = findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myRef.child(key).child("ListOfSubsGames").child(name).removeValue();
+                Intent unsub = new Intent(SubGames.this,Subs.class);
+                startActivity(unsub);
+            }
+        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.home:
+                Toast.makeText(this,"Home selected",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this,MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.movie:
+                Toast.makeText(this,"movie selected",Toast.LENGTH_SHORT).show();
+                Intent intent2 = new Intent(this,Movies.class);
+                startActivity(intent2);
+                break;
+            case R.id.games:
+                Toast.makeText(this,"games selected",Toast.LENGTH_SHORT).show();
+                Intent intent3 = new Intent(this,Games.class);
+                startActivity(intent3);
+                break;
+            case R.id.tvshows:
+                Toast.makeText(this,"tvshows selected",Toast.LENGTH_SHORT).show();
+                Intent intent4 = new Intent(this,TvShows.class);
+                startActivity(intent4);
+                break;
+            case R.id.sub:
+                Toast.makeText(this,"sub selected",Toast.LENGTH_SHORT).show();
+                Intent intent5 = new Intent(this,Subs.class);
+                startActivity(intent5);
+                break;
+            case R.id.logout:
+                Toast.makeText(this, "Loggin out", Toast.LENGTH_SHORT).show();
+                Intent intent6 = new Intent(this,Login.class);
+                startActivity(intent6);
+                break;
+        }
+        return false;
+    }
+
+
+}
